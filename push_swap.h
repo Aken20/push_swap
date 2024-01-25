2dba30d018d979a0b02b0b394290747bc080106a
Aken20 <139094812+Aken20@users.noreply.github.com>
2024-01-25 18:33:48 +0400

    Add files via upload

---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahibrahi <ahibrahi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 15:02:40 by ahibrahi          #+#    #+#             */
+/*   Updated: 2024/01/25 17:00:44 by ahibrahi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+
+typedef struct stack
+{
+	int				data;
+	struct stack	*next;
+}					t_stack;
+
+#endif
